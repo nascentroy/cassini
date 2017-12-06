88288561ffe6c085841beac13d6bf11cd1dc6ebd
nascentroy <nascntroy@gmail.com>
2017-12-06 18:26:02 +0800

    辣鸡条形图

--- ch5.3/index.js
+++ ch5.3/index.js
@@ -1,9 +1,25 @@
-var data = [30, 86, 168, 281, 303, 365];
+ var dataset = [10, 6, 30, 60, 19];
+        d3.select("body").transition()
+            .style("background-color", "teal");
 
-d3.select(".chart")
-  .selectAll("div")
-  .data(data)
-    .enter()
-    .append("div")
-    .style("width", function(d) { return d * 2 + "px"; })
-    .text(function(d) { return '$ ' + d; });
+
+        d3.select(".bar")
+            .selectAll("div")
+            .data(dataset)
+            .enter()
+            .append("div")
+            .attr("class", "bar")
+            .style("width", function(d) {
+                var barwidth = d * 5;
+                return barwidth + "px";
+            })
+            .text(function(d) {
+                return "来吃豆豆: " + d;
+            }) //接收d为输入的匿名函数，
+            .style("color", function(d) {
+                if (d >= 30) {
+                    return "yellow"
+                } else {
+                    return "white"
+                }
+            });
